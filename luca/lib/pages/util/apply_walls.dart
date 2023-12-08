437f8bc39b6053f4590cbf40174fd92a9fa254b5
BatesharXgit <121009508+BatesharXgit@users.noreply.github.com>
2023-12-08 20:31:42 +0530

    confetti added

--- luca/lib/pages/util/apply_walls.dart
+++ luca/lib/pages/util/apply_walls.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:math';
 import 'package:async_wallpaper/async_wallpaper.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,6 +31,8 @@ class ApplyWallpaperPage extends StatefulWidget {
 }
 
 class _ApplyWallpaperPageState extends State<ApplyWallpaperPage> {
+  late ConfettiController _controllerCenter;
+
   final ScrollController _scrollController = ScrollController();
   final GlobalKey _globalKey = GlobalKey();
 
@@ -42,11 +46,12 @@ class _ApplyWallpaperPageState extends State<ApplyWallpaperPage> {
     _createBannerAd();
     _createInterstitialAd();
     // _loadRewardedAd();
+    _controllerCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
   }
 
   BannerAd? _banner;
   InterstitialAd? _interstitialAd;
-  RewardedAd? _rewardedAd;
   void _createBannerAd() {
     _banner = BannerAd(
       size: AdSize.banner,
@@ -139,7 +144,7 @@ class _ApplyWallpaperPageState extends State<ApplyWallpaperPage> {
   @override
   void dispose() {
     _scrollController.dispose();
-
+    _controllerCenter.dispose();
     super.dispose();
   }
 
@@ -215,6 +220,7 @@ class _ApplyWallpaperPageState extends State<ApplyWallpaperPage> {
       // ScaffoldMessenger.of(context).removeCurrentSnackBar();
 
       if (success) {
+        _controllerCenter.play();
         successScreen();
         Fluttertoast.showToast(
           msg: 'Wallpaper set Successfully 😊',
@@ -265,6 +271,7 @@ class _ApplyWallpaperPageState extends State<ApplyWallpaperPage> {
 
       if (success) {
         successScreen();
+        _controllerCenter.play();
         Fluttertoast.showToast(
           msg: 'Wallpaper set Successfully 😊',
           toastLength: Toast.LENGTH_SHORT,
@@ -313,6 +320,7 @@ class _ApplyWallpaperPageState extends State<ApplyWallpaperPage> {
       // ScaffoldMessenger.of(context).removeCurrentSnackBar();
 
       if (success) {
+        _controllerCenter.play();
         successScreen();
         Fluttertoast.showToast(
           msg: 'Wallpaper set Successfully 😊',
@@ -351,18 +359,51 @@ class _ApplyWallpaperPageState extends State<ApplyWallpaperPage> {
     });
   }
 
+  Path drawStar(Size size) {
+    // Method to convert degree to radians
+    double degToRad(double deg) => deg * (pi / 180.0);
+
+    const numberOfPoints = 5;
+    final halfWidth = size.width / 2;
+    final externalRadius = halfWidth;
+    final internalRadius = halfWidth / 2.5;
+    final degreesPerStep = degToRad(360 / numberOfPoints);
+    final halfDegreesPerStep = degreesPerStep / 2;
+    final path = Path();
+    final fullAngle = degToRad(360);
+    path.moveTo(size.width, halfWidth);
+
+    for (double step = 0; step < fullAngle; step += degreesPerStep) {
+      path.lineTo(halfWidth + externalRadius * cos(step),
+          halfWidth + externalRadius * sin(step));
+      path.lineTo(halfWidth + internalRadius * cos(step + halfDegreesPerStep),
+          halfWidth + internalRadius * sin(step + halfDegreesPerStep));
+    }
+    path.close();
+    return path;
+  }
+
   void successScreen() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/success.gif'),
-                  filterQuality: FilterQuality.high,
-                  fit: BoxFit.cover)),
+        return Align(
+          alignment: Alignment.center,
+          child: ConfettiWidget(
+            confettiController: _controllerCenter,
+            blastDirectionality: BlastDirectionality
+                .explosive, // don't specify a direction, blast randomly
+            shouldLoop:
+                true, // start again as soon as the animation is finished
+            colors: const [
+              Colors.green,
+              Colors.blue,
+              Colors.pink,
+              Colors.orange,
+              Colors.purple
+            ], // manually specify the colors to be used
+            createParticlePath: drawStar, // define a custom shape/path.
+          ),
         );
       },
     );
