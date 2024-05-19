f0b21380edfa8e67be155820aff709dfd5a85715
Yogi Bateshar <bateshar.xyz@gmail.com>
2024-05-19 20:42:47 +0530

    adding new home UI with getx

--- luca/lib/pages/homepage.dart
+++ luca/lib/pages/homepage.dart
@@ -1,92 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:luca/controllers/category_controller.dart';
 import 'package:luca/controllers/home_controller.dart';
+import 'package:luca/helpers/ad_helper.dart';
 import 'package:luca/pages/util/apply_walls.dart';
+import 'package:luca/pages/util/components.dart';
 
 import '../models/category_wallpaper.dart';
 
 class MyHomePage extends StatelessWidget {
   final HomeController controller = Get.put(HomeController());
 
+  MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     Color backgroundColor = Theme.of(context).colorScheme.background;
     Color primaryColor = Theme.of(context).colorScheme.primary;
+    Color secondaryColor = Theme.of(context).colorScheme.secondary;
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'LuCa',
-          style: GoogleFonts.lobsterTwo(
-            textStyle: TextStyle(color: primaryColor),
-          ),
-        ),
-        bottom: TabBar(
-          controller: controller.tabController,
-          isScrollable: true,
-          tabs: List<Widget>.generate(
-            controller.data.length,
-            (int index) {
-              return Tab(
-                child: Text(
-                  controller.data[index],
-                  style: GoogleFonts.breeSerif(
-                    textStyle: TextStyle(color: primaryColor),
-                  ),
+      appBar: null,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              TabBar(
+                padding: EdgeInsets.fromLTRB(0, 4, 0, 0),
+                tabAlignment: TabAlignment.start,
+                dividerColor: Colors.transparent,
+                physics: const BouncingScrollPhysics(),
+                indicatorPadding: const EdgeInsets.fromLTRB(0, 42, 0, 2),
+                controller: controller.tabController,
+                indicatorColor: primaryColor,
+                labelPadding: EdgeInsets.only(right: 10, left: 10),
+                indicator: BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.circular(20),
                 ),
-              );
-            },
-          ),
-        ),
-        backgroundColor: backgroundColor,
-        elevation: 5,
-      ),
-      body: TabBarView(
-        controller: controller.tabController,
-        children: List<Widget>.generate(controller.data.length, (index) {
-          if (index == 0) {
-            return Obx(() {
-              if (controller.isLoading.value && controller.wallpapers.isEmpty) {
-                return Center(child: CircularProgressIndicator());
-              }
-              return GridView.builder(
-                controller: controller.scrollController,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.7,
-                  mainAxisSpacing: 5,
-                  crossAxisSpacing: 5,
-                ),
-                itemCount: controller.wallpapers.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) => ApplyWallpaperPage(wallpaper: controller.wallpapers[index]),
-                      //   ),
-                      // );
-                    },
-                    child: CachedNetworkImage(
-                      imageUrl: controller.wallpapers[index].thumbnailUrl,
-                      placeholder: (context, url) =>
-                          CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
-                      fit: BoxFit.cover,
+                labelColor: primaryColor,
+                unselectedLabelColor: secondaryColor,
+                isScrollable: true,
+                tabs: controller.data.map((tab) {
+                  return Tab(
+                    child: Text(
+                      tab,
+                      style: GoogleFonts.montserrat(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   );
-                },
-              );
-            });
-          } else {
-            return CategoryPage(category: controller.data[index]);
-          }
-        }),
+                }).toList(),
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: controller.tabController,
+                  children:
+                      List<Widget>.generate(controller.data.length, (index) {
+                    if (index == 0) {
+                      return Obx(() {
+                        if (controller.isLoading.value &&
+                            controller.wallpapers.isEmpty) {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                        return _buildImageGridFromRef(controller);
+                      });
+                    } else {
+                      return CategoryPage(category: controller.data[index]);
+                    }
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImageGridFromRef(HomeController controller) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+      child: CustomScrollView(
+        controller: controller.scrollController,
+        physics: const ClampingScrollPhysics(),
+        slivers: <Widget>[
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.6,
+              mainAxisSpacing: 6,
+              crossAxisSpacing: 6,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    AdHelper.showInterstitialAd(onComplete: () async {
+                      await Get.to(
+                        ApplyWallpaperPage(
+                          url: controller.wallpapers[index].url,
+                          uploaderName:
+                              controller.wallpapers[index].uploaderName,
+                          title: controller.wallpapers[index].title,
+                          thumbnailUrl:
+                              controller.wallpapers[index].thumbnailUrl,
+                        ),
+                        transition: Transition.downToUp,
+                      );
+                    });
+                  },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: CachedNetworkImage(
+                      fadeInDuration: const Duration(milliseconds: 50),
+                      fadeOutDuration: const Duration(milliseconds: 50),
+                      imageUrl: controller.wallpapers[index].thumbnailUrl,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) =>
+                          Components.buildShimmerEffect(context),
+                    ),
+                  ),
+                );
+              },
+              childCount: controller.wallpapers.length,
+            ),
+          ),
+          if (controller.isLoading.value)
+            SliverToBoxAdapter(
+              child: Center(
+                child: Components.buildPlaceholder(),
+              ),
+            ),
+        ],
       ),
     );
   }
@@ -107,35 +157,70 @@ class CategoryPage extends StatelessWidget {
         return Center(child: CircularProgressIndicator());
       }
 
-      return GridView.builder(
-        controller: controller.scrollController,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 0.7,
-          mainAxisSpacing: 5,
-          crossAxisSpacing: 5,
-        ),
-        itemCount: controller.categoriesWallpapers.length,
-        itemBuilder: (context, index) {
-          CategoryWallpaper wallpaper = controller.categoriesWallpapers[index];
-          return GestureDetector(
-            onTap: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => ApplyWallpaperPage(wallpaper: wallpaper),
-              //   ),
-              // );
-            },
-            child: CachedNetworkImage(
-              imageUrl: wallpaper.thumbnailUrl,
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-              fit: BoxFit.cover,
-            ),
-          );
-        },
-      );
+      return _buildImageGrid(controller);
     });
+  }
+
+  Widget _buildImageGrid(CategoryController controller) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+      child: CustomScrollView(
+        controller: controller.scrollController,
+        physics: const ClampingScrollPhysics(),
+        slivers: <Widget>[
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.6,
+              mainAxisSpacing: 6,
+              crossAxisSpacing: 6,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    AdHelper.showInterstitialAd(onComplete: () async {
+                      await Get.to(
+                        ApplyWallpaperPage(
+                          url: controller.categoriesWallpapers[index].url,
+                          uploaderName: controller
+                              .categoriesWallpapers[index].uploaderName,
+                          title: controller.categoriesWallpapers[index].title,
+                          thumbnailUrl: controller
+                              .categoriesWallpapers[index].thumbnailUrl,
+                        ),
+                        transition: Transition.downToUp,
+                      );
+                    });
+                  },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: CachedNetworkImage(
+                      fadeInDuration: const Duration(milliseconds: 50),
+                      fadeOutDuration: const Duration(milliseconds: 50),
+                      imageUrl:
+                          controller.categoriesWallpapers[index].thumbnailUrl,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+              childCount: controller.categoriesWallpapers.length,
+            ),
+          ),
+          if (controller.isLoading.value)
+            SliverToBoxAdapter(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
